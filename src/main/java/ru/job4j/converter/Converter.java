1d837174fda5bfdb2109bf9654e994b79a7d0797
Ahat <ahatmaster@mail.ru>
2021-12-23 00:15:49 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,32 +1,32 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static float rubleToEuro(float value) {   /* формула перевода рубли в евро */
+    public static float rubleToEuro(float value) {
         float rsl = value / 70;
         return rsl;
     }
 
-    public static float rubleToDollar(float value) {  /* формула перевода рубли в доллары */
+    public static float rubleToDollar(float value) {
         float rsl = value / 60;
         return rsl;
     }
 
-    public static float euroToDollar(float value) {  /* формула пары евро-доллар */
+    public static float euroToDollar(float value) {
         float rsl = value * 1.17F;
         return rsl;
     }
 
-    public static float dollarToEuro(float value) {  /* формула пары доллар-евро */
+    public static float dollarToEuro(float value) {
         float rsl = value * 0.85F;
         return rsl;
     }
 
-    public static float euroToRuble(float value) {  /* формула перевода евро в рубли */
+    public static float euroToRuble(float value) {
         float rsl = value / 0.014285F;
         return rsl;
     }
 
-    public static float dollarToRuble(float value) {  /* формула перевода доллары в рубли */
+    public static float dollarToRuble(float value) {
         float rsl = value / 0.01666F;
         return rsl;
     }
@@ -38,11 +38,11 @@ public class Converter {
         float dollarEuro = Converter.dollarToEuro(15);
         float euroRuble = Converter.euroToRuble(3);
         float dollarRuble = Converter.dollarToRuble(1);
-        System.out.println("140 rubles are " + euro + " euro");       /* перевод рубли в евро */
-        System.out.println("25 rubles are " + dollar + " dollar");    /* перевод рубли в доллары */
-        System.out.println("20 euro are " + euroDollar + " dollar");  /* перевод евро в доллары */
-        System.out.println("15 dollar are " + dollarEuro + " euro");  /* перевод доллары в евро */
-        System.out.println("3 euro are " + euroRuble + " ruble");     /* перевод евро в рубли */
-        System.out.println("1 dollar are " + dollarRuble + " ruble"); /* перевод доллары в рубли */
+        System.out.println("140 rubles are " + euro + " euro");
+        System.out.println("25 rubles are " + dollar + " dollar");
+        System.out.println("20 euro are " + euroDollar + " dollar");
+        System.out.println("15 dollar are " + dollarEuro + " euro");
+        System.out.println("3 euro are " + euroRuble + " ruble");
+        System.out.println("1 dollar are " + dollarRuble + " ruble");
     }
 }
