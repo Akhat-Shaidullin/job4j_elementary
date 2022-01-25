1b84c3dda077dcdb9aeeb42232837d6087cfc0d2
Ahat <ahatmaster@mail.ru>
2022-01-25 18:17:20 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -14,7 +14,7 @@ public class CounterTest {
     }
 
     @Test
-    public void whenSumEvenNumbersFromThreeToEightThenEighteen() {
+    public void sumEvenNumbersFromThreeToEightThenEighteen() {
         int start = 3;
         int finish = 8;
         int result = Counter.sumByEven(start, finish);
